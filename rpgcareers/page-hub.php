a356f2471e4d423c0dbf0838b4ce1174e4f9f778
Ian Holmes <34648982+ianrholmes@users.noreply.github.com>
2018-01-19 11:48:03 +0000

    Quick tidy of hub template

--- rpgcareers/page-hub.php
+++ rpgcareers/page-hub.php
@@ -7,54 +7,9 @@ get_header(); ?>
 			<h1 class="heading-xlarge"><?php the_title(); ?></h1>
 
 			<?php 
-				// STAT BANNER COMPONENT
-				$banner = get_field('departmenthome_banner');
-				$data = array(
-					'heading' => $banner['banner_heading'],
-					'bgimage' => $banner['banner_background_image']
-				);
-
-				include(locate_template('template-parts/components/stat-banner.php'));
+				$hub = get_field('component_hub_intro');
+				echo $hub['component_hub_intro_content'];
 			?>
-
-			<?php 
-				// SEARCH BOX COMPONENT
-				$search = get_field('departmenthome_search_box');
-				$data = array(
-					'label' => $search['departmenthome_search_label'],
-					'explainer' => $search['departmenthome_search_explanation_text']
-				);
-
-				include(locate_template('template-parts/components/search.php'));
-			?>
-
-			<?php 
-				// TEXT / IMAGE COMPONENT
-				$textimage1 = get_field('departmenthome_text_and_image_component_1');
-				$data = array(
-					'heading' => $textimage1['departmenthome_text_and_image_component_1_heading'],
-					'image' => $textimage1['departmenthome_text_and_image_component_1_image'],
-					'intro' => $textimage1['departmenthome_text_and_image_component_1_intro_text'],
-					'content' => $textimage1['departmenthome_text_and_image_component_1_content']
-				);
-
-				include(locate_template('template-parts/components/text-image.php'));
-			?>
-
-			<?php 
-				// DEPARTMENT LIST COMPONENT
-				$depts = get_field('dept_info');
-				$data = array(
-					'heading' => $depts['dept_info_heading'],
-					'intro' => $depts['dept_info_sub_heading'],
-					'department_fieldname' => 'dept_info_department_list',
-					'departments' => $depts['dept_info_department_list'],
-					'departments_link' => $depts['link_to_list_of_departments']
-				);
-
-				include(locate_template('template-parts/components/department-list.php'));
-			?>
-
 	</div>
 <?php
 get_footer();
